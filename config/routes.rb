242e3c1195af01ac23b6200dc050c1b6bff0f26c
Katarzyna Bernad <katarzyna.bernad@netguru.co>
2019-02-23 13:42:48 +0100

    introduce calendar with tasks - monthly page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'planner#dashboard', as: :dashboard
+  get "/monthly", to: "planner#monthly", as: :monthly
+  get "/by_category", to: "planner#by_category", as: :by_category
 end
